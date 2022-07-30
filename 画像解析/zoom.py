8b8013c5de0999c8312080e7f4ea0ce16fee80a4
N Tanaka <66735232+NTanaka1994@users.noreply.github.com>
2022-07-30 16:22:37 +0900

    Update zoom.py

--- 画像解析/zoom.py
+++ 画像解析/zoom.py
@@ -34,6 +34,8 @@ def shape(imary):
         for x in range(len(imary[y])-2):
             if dst[y][x] > 255:
                dst[y][x] = 255
+            if dst[y][x] < 0:
+                dst[y][x] = 0
     return dst.astype(int)
 
 #雑音除去
@@ -104,25 +106,31 @@ def noise(imary):
             imary[y][x] = np.median(tmp)
     return imary
 
-#拡大鮮明化
-def zoom_clear(imary):
-    mask = np.zeros((len(imary), len(imary[0])))
-    tmp1 = np.zeros((2*len(imary), 2*len(imary[0])))
-    #imary = contrast(imary)
+#拡大鮮明化(形の鮮明化→バイリニア補完法→色の鮮明化→ノイズ除去)
+def zoom_clear(imary, height, width):
+    tmp1 = np.zeros((height, width))
     imary = shape(imary)
-    for y in range(len(imary)):
-        for x in range(len(imary[y])):
-            tmp1[y*2][x*2] = imary[y][x]
-    for y in range(len(tmp1)-1):
-        for x in range(len(tmp1[y])-1):
-            if (y % 2) == 0:
-                if (x % 2) == 1 and x != (len(tmp1[y])-1):
-                    tmp1[y][x] = (tmp1[y][x-1] + tmp1[y][x+1]) / 2
-    for y in range(len(tmp1)-1):
-        for x in range(len(tmp1[y])-1):
-            if (y % 2) == 1 and y != (len(tmp1)-1):
-                tmp1[y][x] = (tmp1[y-1][x] + tmp1[y+1][x]) / 2
-    #tmp1 = shape(tmp1.astype(int))
+    h = imary.shape[0]
+    w = imary.shape[1]
+    ax = width / w
+    ay = height / h
+    for yd in range(height):
+        for xd in range(width):
+            x = xd / ax
+            y = yd / ay
+            ox = int(x)
+            oy = int(y)
+            if ox > (w - 2):
+                ox = w - 2
+            if oy > (h - 2):
+                oy = h - 2
+            dx = x - ox
+            dy = y - oy
+            tmp1[yd][xd] = (1 - dx) * (1 - dy) * imary[oy][ox] + dx * (1 - dy) * imary[oy][ox+1] + (1 - dx) * dy * imary[oy][ox+1] + dx * dy * imary[oy+1][ox+1]
+            if tmp1[yd][xd] > 255:
+                tmp1[yd][xd] = 255
+            if tmp1[yd][xd] < 0:
+                tmp1[yd][xd] = 0
     tmp1 = contrast(tmp1.astype(int))
     tmp1 = noise(tmp1)
     return tmp1.astype(int)
@@ -132,9 +140,8 @@ def zoom_clear(imary):
 image = Image.open("Lenna.png").convert("L")
 image.convert("RGB").save("mono.png")
 imary = np.array(image)
-tmp = zoom_clear(imary)
-#tmp = contrast(tmp)
+tmp = zoom_clear(imary, imary.shape[1]*3, imary.shape[0]*3)
 clrim = Image.fromarray(tmp).convert("RGB")
-clrim.save("dst/clear3.png")
+clrim.save("dst/clea4.png")
 plt.imshow(tmp, cmap="gray")
 plt.show()
