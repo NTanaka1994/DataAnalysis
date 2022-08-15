015015c727285ba0530e1f3fb9a4d1baea7b5a22
N Tanaka <66735232+NTanaka1994@users.noreply.github.com>
2022-08-15 19:57:19 +0900

    Update Json_Analysis.py

--- JSONの解析/Json_Analysis.py
+++ JSONの解析/Json_Analysis.py
@@ -1,5 +1,6 @@
 import json
 import requests
+import pandas as pd
 
 def depth(jsn, var=""):
     if isinstance(jsn, dict):
@@ -13,47 +14,49 @@ def depth(jsn, var=""):
         pass
 
 def width(jsn, ed=[]):
-    root = []
+    ptmp = []
+    path = []
     if isinstance(jsn, dict):
         for row in jsn:
             ed.append(row)
-        root.append(ed.copy())
+            ptmp.append(row)
+        path.append(ptmp)
+        ptmp = []
     elif isinstance(jsn, list):
         for i in range(len(jsn)):
             ed.append(i)
-        root.append(ed.copy())
+            ptmp.append(i)
+        path.append(ptmp)
+        ptmp = []
     tmp = []
     while len(ed) > 0:
         node = ed[0]
-        if len(ed) == 1:
-            if isinstance(node, str):
-                print("[\""+node+"\"]", end="")
-            else:
-                print("["+str(node)+"]", end="")
-        else:
-            if isinstance(node, str):
-                print("[\""+node+"\"]")
-            else:
-                print("["+str(node)+"]")
         del ed[0]
         if tmp == []:
             jsn = jsn[node]
         else:
             jsn = tmp[0][node]
             del tmp[0]
+        ptmp.append(node)
         if isinstance(jsn, dict):
             for row in jsn:
                 tmp.append(jsn)
+                ptmp.append(row)
                 ed.append(row)
-            root.append(ed.copy())
+            path.append(ptmp)
+            ptmp = []
         elif isinstance(jsn, list) and jsn != []:
             for i in range(len(jsn)):
                 tmp.append(jsn)
+                ptmp.append(i)
                 ed.append(i)
-            root.append(ed.copy())
+            path.append(ptmp)
+            ptmp = []
         else:
-            print("="+str(jsn))
-    pass
+            ptmp.append("END:"+str(jsn))
+            path.append(ptmp)
+            ptmp = []
+    return path
 
 url = "https://raw.githubusercontent.com/NTanaka1994/DataAnalysis/main/JSON%E3%81%AE%E8%A7%A3%E6%9E%90/json_sample.json" #ここにJSONのあるURLを入れる
 
@@ -62,4 +65,14 @@ res = requests.get(url)
 data = json.loads(res.text)
 
 depth(data, "data")
-width(data)
+path = width(data)
+df = pd.DataFrame(path)
+col = []
+for i in range(len(df.columns)):
+    if i == 0:
+        col.append("親ノード")
+    else:
+        col.append("子ノード"+str(i))
+df.columns = col
+print(df)
+df.to_csv("result.csv", encoding="shift-jis", index=False)
